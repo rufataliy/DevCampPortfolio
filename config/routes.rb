74f5d91c4a1d1ac3eb2626a493768b7cfe24392f
rufataliy <rufataliyevbakou@gmail.com>
2019-08-21 16:47:37 -0400

    custom routes for portfolio item

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  resources :portfolios, except: [:show]
+  get "portfolio/:id", to: "portfolios#show"
+  get 'home', to: "pages#home"
+  get 'about', to: "pages#about"
+  get 'contact', to: "pages#contact"
   resources :blogs
-  resources :skills
+  root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
