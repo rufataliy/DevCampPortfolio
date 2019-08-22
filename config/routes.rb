aa162a419663ca8f485b3d82796e952e2eddca4d
rufataliy <rufataliyevbakou@gmail.com>
2019-08-22 17:37:14 -0400

    creating data relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
-  get "portfolio/:id", to: "portfolios#show"
+  get "portfolios/:id", to: "portfolios#show" , as: "portfolio_id"
   get 'home', to: "pages#home"
   get 'about', to: "pages#about"
   get 'contact', to: "pages#contact"
