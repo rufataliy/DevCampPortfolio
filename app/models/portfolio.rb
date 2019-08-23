3eeb72154f306ed45e2f58b4e1aa8f2a651dfd60
rufataliy <rufataliyevbakou@gmail.com>
2019-08-22 21:44:31 -0400

    implemented custome scopes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,9 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title, :body, :main_image, :thumb_image
+
+    def self.angular
+        where(subtitle: "Angular")
+    end
+
+    scope :react_portfolio_item, -> {where(subtitle: "React")}
 end
