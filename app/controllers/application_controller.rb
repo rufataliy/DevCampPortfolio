00841dbc113aafeed22cb971b951f50cad01a780
rufataliy <rufataliyevbakou@gmail.com>
2019-08-24 11:47:06 -0400

    added session source tracker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
    include DeviseWhitelist
+
+   before_action :source_session
+   def source_session
+      session[:source]=params[:q] if params[:q]
+   end
 end
