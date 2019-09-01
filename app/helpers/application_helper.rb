e40b4557eeb2061bfe9cc35d994144d5d32bf35d
rufataliy <rufataliyevbakou@gmail.com>
2019-09-01 13:04:06 -0400

    Addes styled portfolio template to portfolios and _portfolio_item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,13 @@ module ApplicationHelper
 
     def login_helper
       if current_user.is_a?(GuestUser)
-        (link_to  "log in", new_user_session_path )+
-        "<br>".html_safe+
-         (link_to  "register", new_user_registration_path)
+        "<li class='sidebar-nav-item'>".html_safe+
+          (link_to  "Log in", new_user_session_path , class: "js-scroll-trigger")+
+        "</li>".html_safe+
+       
+        "<li class='sidebar-nav-item'>".html_safe+
+          (link_to  "Register", new_user_registration_path, class: "js-scroll-trigger")+
+        "</li>".html_safe
        else
         link_to  "log out", destroy_user_session_path, method: :delete
         
